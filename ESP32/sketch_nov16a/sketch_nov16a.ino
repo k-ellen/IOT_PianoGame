f3772674a70cc673e0a01d52a9c8bd0800d22bd3
Yarden keshet <yardenkeshet@gmail.com>
2025-11-16 16:49:30 +0200

    base code

---
+++ ESP32/sketch_nov16a/sketch_nov16a.ino
@@ -0,0 +1,81 @@
+
+
+#include <MIDI.h>
+
+// Use Serial1 for MIDI-IN (pins 19=RX, 18=TX, but TX not used for MIDI-IN)
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
+
+void handleNoteOn(byte channel, byte note, byte velocity) {
+    Serial.print("Note On: ");
+    Serial.println(note);
+}
+
+void handleNoteOff(byte channel, byte note, byte velocity) {
+    Serial.print("Note Off: ");
+    Serial.println(note);
+}
+
+void setup() {
+    Serial.begin(115200);       // Debug output
+    Serial1.begin(31250);       // MIDI baud
+    MIDI.begin(MIDI_CHANNEL_OMNI);  // Listen to all channels
+
+    MIDI.setHandleNoteOn(handleNoteOn);
+    MIDI.setHandleNoteOff(handleNoteOff);
+
+    Serial.println("MIDI ready on Mega with HardwareSerial");
+}
+
+void loop() {
+    MIDI.read();  // Always call as often as possible
+    // if (MIDI.read()) {
+    //     Serial.print("Type: "); Serial.print(MIDI.getType());
+    //     Serial.print(" Channel: "); Serial.print(MIDI.getChannel());
+    //     Serial.print(" Note: "); Serial.print(MIDI.getData1());
+    //     Serial.print(" Velocity: "); Serial.println(MIDI.getData2());
+    // }
+}
+
+
+
+// #include <MIDI.h>
+// // #include <SoftwareSerial.h>
+// #include <AltSoftSerial.h>
+
+// // SoftwareSerial midiSerial(2, 3); // RX, TX
+// AltSoftSerial midiSerial;  // RX=8, TX=9 (TX not used)
+
+// MIDI_CREATE_INSTANCE(AltSoftSerial, midiSerial, MIDI);
+
+// void handleNoteOn(byte channel, byte note, byte velocity) {
+//   Serial.println("note on");
+// }
+
+// void handleNoteOff(byte channel, byte note, byte velocity) {
+//   Serial.println("note off");
+// }
+
+
+// void setup()
+// {
+//   // Open serial communications and wait for port to open:
+//   Serial.begin(115200);
+//   Serial.println("hardware serial ready");
+
+//     // Start MIDI
+//   midiSerial.begin(31250);
+//   MIDI.begin(MIDI_CHANNEL_OMNI);
+
+//   // Set callbacks
+//   MIDI.setHandleNoteOn(handleNoteOn);
+//   MIDI.setHandleNoteOff(handleNoteOff);
+
+//   Serial.println("MIDI ready...");
+// }
+
+// void loop() // run over and over
+// {
+//   MIDI.read();
+// }
+
+
