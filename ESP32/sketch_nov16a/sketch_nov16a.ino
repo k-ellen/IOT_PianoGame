b4b6eba959ad25a183f100e42b8141a1de19898c
k-ellen <158521238+k-ellen@users.noreply.github.com>
2025-11-25 14:49:03 +0200

    Test arduino and notes using MIDI library

--- ESP32/sketch_nov16a/sketch_nov16a.ino
+++ ESP32/sketch_nov16a/sketch_nov16a.ino
@@ -1,37 +1,59 @@
+#include <MIDI.h>
+
+// If using Uno/Nano, use Serial: MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDI);
+// If using Mega (which has Serial1), use Serial1 (pins 19/18):
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI); 
 
 
-// #include <MIDI.h>
+void handleNoteOn(byte channel1, byte note1, byte velocity1) {
+    // Serial.print("Note On: ");
+    // Serial.println(note);
+    byte type = MIDI.getType();
+    byte channel = MIDI.getChannel();
+    byte data1 = MIDI.getData1(); // Note Number
+    byte data2 = MIDI.getData2(); // Velocity
 
-// // Use Serial1 for MIDI-IN (pins 19=RX, 18=TX, but TX not used for MIDI-IN)
-// MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
-
-// void handleNoteOn(byte channel, byte note, byte velocity) {
-//     Serial.print("Note On: ");
-//     Serial.println(note);
-// }
+    // --- Print MIDI Type/Status in Hex ---
+    Serial.print("Type: 0x"); 
+    Serial.print(type, HEX); 
+        
+    Serial.print(" Ch: "); 
+    Serial.print(channel);
+        
+    // --- Print Data 1 (Note) in Decimal AND Hex ---
+    Serial.print(" D1 (Note): "); 
+    Serial.print(data1);            // Decimal
+    Serial.print(" (0x");
+    Serial.print(data1, HEX);       // Hexadecimal
+    Serial.print(")");
+        
+    // --- Print Data 2 (Velocity) in Decimal AND Hex ---
+    Serial.print(" D2 (Vel): "); 
+    Serial.print(data2);            // Decimal
+    Serial.print(" (0x");
+    Serial.print(data2, HEX);       // Hexadecimal
+    Serial.println(")");
+}
 
 // void handleNoteOff(byte channel, byte note, byte velocity) {
 //     Serial.print("Note Off: ");
 //     Serial.println(note);
 // }
 
-// void setup() {
-//     Serial.begin(115200);       // Debug output
-//     Serial1.begin(31250);       // MIDI baud
-//     MIDI.begin(MIDI_CHANNEL_OMNI);  // Listen to all channels
+void setup() {
+    Serial.begin(115200);       // Debug output
+    Serial1.begin(31250);       // MIDI baud
+    MIDI.begin(MIDI_CHANNEL_OMNI);  // Listen to all channels
 
-//     MIDI.setHandleNoteOn(handleNoteOn);
-//     MIDI.setHandleNoteOff(handleNoteOff);
+    MIDI.setHandleNoteOn(handleNoteOn);
+    // MIDI.setHandleNoteOff(handleNoteOff);
 
-//     Serial.println("MIDI ready on Mega with HardwareSerial");
-// }
+    Serial.println("MIDI ready on Mega with HardwareSerial");
+}
 
-// void loop() {
-//     MIDI.read();  // Always call as often as possible
-//     // if (MIDI.read()) {
-//     //     Serial.print("Type: "); Serial.print(MIDI.getType());
-//     //     Serial.print(" Channel: "); Serial.print(MIDI.getChannel());
-//     //     Serial.print(" Note: "); Serial.print(MIDI.getData1());
-//     //     Serial.print(" Velocity: "); Serial.println(MIDI.getData2());
-//     // }
-// }
+
+void loop() {
+    // Check for a new MIDI message
+    MIDI.read();
+
+}
