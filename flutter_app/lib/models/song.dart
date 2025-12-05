3e8af5734bd1d6556483d5c77f50f12ef90e7367
MayanLavi <mayan.lavi@campus.technion.ac.il>
2025-12-05 22:59:33 +0200

    add song dart

--- flutter_app/lib/models/song.dart
+++ flutter_app/lib/models/song.dart
@@ -1,18 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-enum SongSource { internet, global, private }
+enum SongSource { global, private }
 
 class Song {
   final String id;
   final String name;
+  final String artist;
   final String source; 
-  final String artist; 
+  final String genre; 
 
   Song({
     required this.id,
     required this.name,
+    required this.artist, 
     required this.source,
-    required this.artist,
+    required this.genre,
   });
 
   factory Song.fromDoc(DocumentSnapshot doc) {   //fromDoc takes a document from Firestore and turns it into a Song object
@@ -20,8 +22,9 @@ class Song {
     return Song(
       id: doc.id,
       name: data['name'] ?? 'Unknown',
+      artist: data['artist'] ?? 'Unknown artist', 
       source: data['source'] ?? 'global',
-      artist: data['artist'] ?? 'Unknown artist',
+      genre: data['genre'] ?? 'Unknown',
     );
   }
 }
