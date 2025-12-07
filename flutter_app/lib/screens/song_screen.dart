0c3179400ffc9fdf52fce3f7b9f8a699982337d3
Yarden Keshet <yardenkeshet@gmail.com>
2025-12-07 13:28:39 +0200

    adding style to tool button and empty song screen

--- flutter_app/lib/screens/song_screen.dart
+++ flutter_app/lib/screens/song_screen.dart
@@ -234,7 +234,7 @@ class _UploadScreenState extends State<UploadScreen> {
         ),
       ),
 
-      bottomNavigationBar: const MyBottomNavigationBar(currentIndex: 2),
+      bottomNavigationBar: const MyBottomNavigationBar(currentIndex: 3),
     );
   }
 }
